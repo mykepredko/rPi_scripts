b8c799ce7d23e0fd0325faea31c04e6018d02380
Michael Predko <myke.predko@mimetics.ca>
2023-08-25 01:15:45 -0400

    Update rPi-setup-2.sh
    Added "-y" to "apt" commands to automatically answer "Yes" to any questions.

--- rPi-setup-2.sh
+++ rPi-setup-2.sh
@@ -14,11 +14,11 @@ echo_yellow(){
     echo -e "\e[33m${1}\e[0m"
 }
 
-echo_yellow "sudo apt update"
-sudo apt update
-echo_yellow "sudo apt list --upgradable"
-sudo apt list --upgradable
-echo_yellow "sudo apt full-upgrade"
-sudo apt full-upgrade
-echo_yellow "sudo apt install git"
-sudo apt install git
+echo_yellow "sudo apt update -y"
+sudo apt update -y
+echo_yellow "sudo apt list --upgradable -y"
+sudo apt list --upgradable -y
+echo_yellow "sudo apt full-upgrade -y"
+sudo apt full-upgrade -y
+echo_yellow "sudo apt install git -y"
+sudo apt install git -y
