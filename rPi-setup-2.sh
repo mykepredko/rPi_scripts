d8339e3347d17f5792d5b348c0680e50b03f18ba
Michael Predko <myke.predko@mimetics.ca>
2023-08-24 23:59:30 -0400

    Update rPi-setup-2.sh
    Add "Echo Colour Specifications"

--- rPi-setup-2.sh
+++ rPi-setup-2.sh
@@ -1,3 +1,19 @@
+#!/usr/bin/env bash
+
+## Echo Colour Specifications
+echo_green(){
+    echo -e "\e[32m${1}\e[0m"
+}
+echo_red(){
+    echo -e "\e[31m${1}\e[0m"
+}
+echo_blue(){
+    echo -e "\e[34m${1}\e[0m"
+}
+echo_yellow(){
+    echo -e "\e[33m${1}\e[0m"
+}
+
 echo_yellow sudo apt update
 sudo apt update
 echo_yellow sudo apt list --upgradable
