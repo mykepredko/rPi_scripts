1659d6f0f03b52fc68bf339ca1a41dba835fec61
Michael Predko <myke.predko@mimetics.ca>
2023-08-24 23:55:07 -0400

    Add files via upload
    Saved in NotePad++ as a Linux file EOL
    Added "echo_yellow" comments so execution can be followed

---
+++ rPi-setup-2.sh
@@ -0,0 +1,8 @@
+echo_yellow sudo apt update
+sudo apt update
+echo_yellow sudo apt list --upgradable
+sudo apt list --upgradable
+echo_yellow sudo apt full-upgrade
+sudo apt full-upgrade
+echo_yellow sudo apt install git
+sudo apt install git
